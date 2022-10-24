bc237e6193b28b843c36c131b2ac804c286796a3
Kolade1024 <akoladeemmanuel14@gmail.com>
2022-10-24 13:31:36 -0700

    3-puts.c

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _puts - prints a string
+ * @str: strings to be printed
+ */
+void _puts(char *str)
+{
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+	_putchar('\n');
